8022dc8fb5a575e385f7b58660daadf01e551bf8
nsaimk <nsaimkorkmaz1@gmail.com>
2023-12-19 10:24:34 +0000

    newpost props for onchange

--- src/components/list/List.jsx
+++ src/components/list/List.jsx
@@ -1,13 +1,25 @@
 import Post from "../post/Post"
 import style from "./List.module.css"
 import NewPost from "../newpost/NewPost"
+import { useState } from "react"
 
 const List = () => {
+
+    const [bodyValue, setBodyValue] = useState('')
+    const [textValue, setTextValue] = useState('')
+
+    const handleBody = (event) => {
+        setBodyValue(event.target.value)
+    }
+    const handleText = (event) => {
+        setTextValue(event.target.value)
+    }
+
     return (
         <>
-            <NewPost />
+            <NewPost onBodyChange={handleBody} onTextChange={handleText}/>
             <ul className={style.post}>
-                <Post name='Bob Marley' body='Dur be Arif!' />
+                <Post name={bodyValue} body={textValue} />
                 <Post name='Ibrahim' body='Bi numara yaptigim yok, o kerizlik bende dogustan beri var.' />
             </ul>
         </>
--- src/components/newpost/NewPost.jsx
+++ src/components/newpost/NewPost.jsx
@@ -1,24 +1,15 @@
 import style from "./NewPost.module.css"
-import { useState } from "react"
 
-const NewPost = () => {
-
-    const [textValue, setTextValue] = useState('')
-
-    const handleBody = (event) => {
-        setTextValue(event.target.value)
-    }
-
+const NewPost = (props) => {
     return(
         <form className={style.form}>
             <p>
                 <label htmlFor="body">Text</label>
-                <textarea id="body" required rows={3} onChange={handleBody}></textarea>
+                <textarea id="body" required rows={3} onChange={props.onTextChange}></textarea>
             </p>
-            <p>{textValue}</p>
             <p>
                 <label htmlFor="name">Your name</label>
-                <textarea type="text" id="name" required rows={1}></textarea>
+                <textarea type="text" id="name" required rows={1} onChange={props.onBodyChange}></textarea>
             </p>
         </form>
     )
